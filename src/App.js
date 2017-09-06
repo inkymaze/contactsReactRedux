753934de79e2d5dab7d0b0e29d569eab3337830c
inkymaze <nickwcannon@gmail.com>
2017-09-06 09:52:47 -0700

    map and render contact list

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListContacts from './ListContacts';
 
-//
-// const people = [
-//   { name: "Nick" },
-//   { name: "Jon" },
-//   { name: "Julia" },
-// ];
-
-class ContactList extends React.Component {
-   render() {
-     const people = this.props.contacts;
-
-     return <ol>
-       {people.map(person => (
-         <li key={person.name}>{person.name}</li>
-       ))}
-     </ol>;
-   }
- }
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: "Nick" },
-          { name: "Jon" },
-          { name: "Julia" },
-        ]} />
+      <div>
+        <ListContacts contacts={contacts}/>
       </div>
     );
   }
