971430a92252ede43932998e9367016e6ce53983
inkymaze <nickwcannon@gmail.com>
2017-09-06 10:54:42 -0700

    refractor state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,31 @@
 import React from 'react';
 
-class ListContacts extends React.Component {
-  render() {
-    console.log();
-    return (
-      <ol className='contact-list' >
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}/>
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className='contact-remove'>
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
-    );
-  }
+// stateless functional component because it only has the render method
+function ListContacts(props) {
+  return (
+    <ol className='contact-list' >
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className='contact-list-item'>
+          <div className='contact-avatar' style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }}/>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button className='contact-remove'>
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
 }
+// class ListContacts extends React.Component {
+//   render() {
+//     console.log();
+//
+//   }
+// }
 
 export default ListContacts;
